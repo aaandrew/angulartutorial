81c31e91c08e69f3b231489149225801b4b7d0c3
Andrew <andrewdude28@gmail.com>
2015-01-25 21:43:53 -0800

    step 4

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -23,14 +23,31 @@ describe('PhoneCat App', function(){
 	      expect(phoneList.count()).toBe(2);
 		});
 
- 		it('should display the current filter value in the title bar', function() {
- 			var query = element(by.model('query'));
-      query.clear();
-      expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+		it('should be possible to control phone order via the drop down select box', function(){
+			var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
+			var query = element(by.model('query'));
 
-      query.sendKeys('nexus');
-      expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
-    });
+			function getNames(){
+				return phoneNameColumn.map(function(elm){
+					return elm.getText();
+				});
+			}
+
+			query.sendKeys('tablet');
+
+			expect(getNames()).toEqual([
+				"Motorola XOOM\u2122 with Wi-Fi",
+    		"MOTOROLA XOOM\u2122"
+			]);
+
+			element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+
+			expect(getNames()).toEqual([
+				"MOTOROLA XOOM\u2122",
+    		"Motorola XOOM\u2122 with Wi-Fi"
+			]);
+
+		});
 
 	});
 });
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,18 +1,26 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
-
 describe('PhoneCat controllers', function() {
 
-	describe('PhoneListCtrl', function(){
-		beforeEach(module('phonecatApp'));
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl;
 
-		it('should create "phones" model with 3 phones', inject(function($controller){
-			var scope = {},
-				ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    beforeEach(module('phonecatApp'));
 
-			expect(scope.phones.length).toBe(3);
-		}));
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    }));
 
-	});
+
+    it('should create "phones" model with 3 phones', function() {
+      expect(scope.phones.length).toBe(3);
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+  });
 });
