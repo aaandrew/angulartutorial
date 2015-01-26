3997ecfe19a2bcc0337cf0d58725c26c0690df77
Andrew <andrewdude28@gmail.com>
2015-01-25 22:26:27 -0800

    step 6

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,52 +2,65 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('PhoneCat App', function(){
-	describe('Phone list view', function(){
-		beforeEach(function(){
-			browser.get('app/index.html');
-		});
+describe('PhoneCat App', function() {
 
-		it('should filter the phone list as a user types into the search box', function() {
+  describe('Phone list view', function() {
 
-	      var phoneList = element.all(by.repeater('phone in phones'));
-	      var query = element(by.model('query'));
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
 
-	      expect(phoneList.count()).toBe(3);
 
-	      query.sendKeys('nexus');
-	      expect(phoneList.count()).toBe(1);
+    it('should filter the phone list as a user types into the search box', function() {
 
-	      query.clear();
-	      query.sendKeys('motorola');
-	      expect(phoneList.count()).toBe(2);
-		});
+      var phoneList = element.all(by.repeater('phone in phones'));
+      var query = element(by.model('query'));
 
-		it('should be possible to control phone order via the drop down select box', function(){
-			var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
-			var query = element(by.model('query'));
+      expect(phoneList.count()).toBe(20);
 
-			function getNames(){
-				return phoneNameColumn.map(function(elm){
-					return elm.getText();
-				});
-			}
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
 
-			query.sendKeys('tablet');
+      query.clear();
+      query.sendKeys('motorola');
+      expect(phoneList.count()).toBe(8);
+    });
 
-			expect(getNames()).toEqual([
-				"Motorola XOOM\u2122 with Wi-Fi",
-    		"MOTOROLA XOOM\u2122"
-			]);
 
-			element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+    it('should be possible to control phone order via the drop down select box', function() {
 
-			expect(getNames()).toEqual([
-				"MOTOROLA XOOM\u2122",
-    		"Motorola XOOM\u2122 with Wi-Fi"
-			]);
+      var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
+      var query = element(by.model('query'));
 
-		});
+      function getNames() {
+        return phoneNameColumn.map(function(elm) {
+          return elm.getText();
+        });
+      }
 
-	});
+      query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
+
+      expect(getNames()).toEqual([
+        "Motorola XOOM\u2122 with Wi-Fi",
+        "MOTOROLA XOOM\u2122"
+      ]);
+
+      element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+
+      expect(getNames()).toEqual([
+        "MOTOROLA XOOM\u2122",
+        "Motorola XOOM\u2122 with Wi-Fi"
+      ]);
+    });
+
+
+    it('should render phone specific links', function() {
+      var query = element(by.model('query'));
+      query.sendKeys('nexus');
+      element(by.css('.phones li a')).click();
+      browser.getLocationAbsUrl().then(function(url) {
+        expect(url.split('#')[1]).toBe('/phones/nexus-s');
+      });
+    });
+  });
 });
