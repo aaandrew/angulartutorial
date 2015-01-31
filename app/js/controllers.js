cf321c7e825f365f7f74c0a2107a2c7af9293be0
Andrew <andrewdude28@gmail.com>
2015-01-30 19:47:13 -0800

    step 10

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -18,5 +18,11 @@ phonecatControllers.controller('PhoneDetailCtrl',
 	function($scope, $routeParams, $http) {
   	$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data){
   		$scope.phone = data;
+  		$scope.mainImageUrl = data.images[0];
   	});	
+
+  	$scope.setImage = function(imageUrl){
+  		$scope.mainImageUrl = imageUrl;
+  	};
+
   }]);
--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -81,7 +81,19 @@ describe('PhoneCat App', function() {
 
     it('shoudl display nexus-s page', function(){
       expect(element(by.binding('phone.name')).getText()).toBe('Nexus S');
-    })
+    });
+
+    it('should display the first phoen image as the main phone image', function(){
+      expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
+    });
+
+    it('should swap main image if a thumbnail image is clicked on', function(){
+      element(by.css('.phone-thumbs li:nth-child(3) img')).click();
+      expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.2.jpg/);
+
+      element(by.css('.phone-thumbs li:nth-child(1) img')).click();
+      expect(element(by.css('img.phone')).getAttribute('src')).toMatch(/img\/phones\/nexus-s.0.jpg/);
+    });
    
   });
 });
